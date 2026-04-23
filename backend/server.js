624f365c2e3ed52f59ac40d6b8fa3f1f06414d33
anshmishra4444 <ansh11072004@gmail.com>
2026-04-23 20:54:17 +0530

    Fix dependency conflicts and lockfile

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,10 @@ const dns = require('node:dns').promises;
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: "https://symptom-based-disease-prediction-14jrhwh25.vercel.app" 
+}));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
